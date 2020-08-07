14cb1ae208942f0ecbb5a60857ac323ca8242271
James <james.lee-1@students.plymouth.ac.uk>
2020-08-07 12:36:41 +0100

    Created a registration GUI on a tabbed panel. Will expand this to include future features.

---
+++ src/view/gui.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.*;
+
+public class gui {
+    private JPanel mainPanel;
+    private JTabbedPane tabbedPane1;
+    private JTextField textField1;
+    private JButton button1;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+}
