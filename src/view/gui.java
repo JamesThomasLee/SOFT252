464111b4895a08b36df54f27be81da99d1d6083b
James <james.lee-1@students.plymouth.ac.uk>
2020-08-07 14:03:36 +0100

    Updated registration form to include main method allowing it to be compiled.

--- src/view/gui.java
+++ src/view/gui.java
@@ -5,16 +5,26 @@ import javax.swing.*;
 public class gui {
     private JPanel mainPanel;
     private JTabbedPane tabbedPane1;
-    private JTextField textField1;
-    private JButton button1;
-    private JPasswordField passwordField1;
-    private JPasswordField passwordField2;
+    private JButton btnReg;
+    private JTextField txtForename;
+    private JTextField txtUserType;
+    private JTextField txtSurname;
+    private JTextField txtAddress;
+    private JTextField txtCity;
+    private JTextField txtCounty;
+    private JTextField txtPostcode;
+    private JTextField txtTel;
+    private JTextField txtEmail;
+    private JPasswordField txtPassword;
+    private JPasswordField txtConfPassword;
+    private JTextField txtUsername;
+    private JButton btnSignIn;
 
     public static void main(String[] args){
         JFrame frame = new JFrame("Patient Management System");
         frame.setContentPane(new gui().mainPanel);
+        frame.setSize(800, 800);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-        frame.pack();
         frame.setVisible(true);
     }
 
