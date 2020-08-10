1005d8bb7f2ac7f8d090a8e93fd0cd0cd4a6adda
James <james.lee-1@students.plymouth.ac.uk>
2020-08-10 13:51:53 +0100

    Updated user class

--- src/users/user.java
+++ src/users/user.java
@@ -8,18 +8,35 @@ public class user implements Serializable{
     protected String firstName;
     protected String surname;
     protected String address;
-    protected IState state;
+    protected String city;
+    protected String county;
+    protected String postcode;
+    protected int tel;
+    protected String email;
+    //protected IState state;
 
-    public String getUserType(){
+    public String getUserType() {
         return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
     }
 
     public String getUserID() {
         return userID;
     }
 
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getFirstName() {
@@ -34,10 +51,6 @@ public class user implements Serializable{
         return surname;
     }
 
-    public void setUserType(String userType){
-        this.userType = userType;
-    }
-
     public void setSurname(String surname) {
         this.surname = surname;
     }
@@ -46,23 +59,58 @@ public class user implements Serializable{
         return address;
     }
 
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public void setAddress(String address) {
         this.address = address;
     }
 
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /*
     public IState getState() {
+
         return state;
     }
 
     public void setState(IState state) {
         this.state = state;
     }
+    */
 }
