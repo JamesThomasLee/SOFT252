1311c57c40057e9e8b3b719aca8e8f4e5348c34a
James Lee <james.lee-1@students.plymouth.ac.uk>
2020-08-26 15:37:39 +0100

    Added javadoc to log out controller.

--- src/controller/logoutController.java
+++ src/controller/logoutController.java
@@ -3,8 +3,15 @@ package controller;
 import view.gui;
 import javax.swing.*;
 
+/**
+ * A controller to handle a user logging out.
+ */
 public class logoutController {
 
+    /**
+     * Logging out function to log out a user.
+     * @param gui - gui
+     */
     public static void logoutUser(gui gui){
         int confirm = JOptionPane.YES_NO_OPTION;
         int outcome = JOptionPane.showConfirmDialog(null, "Are you sure you would like to log out?", "Are you sure?", confirm);
